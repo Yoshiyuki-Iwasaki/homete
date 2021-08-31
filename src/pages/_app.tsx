0b96e98c5364a20d959fa3ff812fbe8fc4aaed43
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-09-01 08:59:47 +0900

    remove unnecessary file and text.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import '../../styles/reset.scss';
-import '../../styles/styles.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import CommonMeta from '../components/CommonMeta';
-import Header from '../components/Header';
 import PostList from '../components/PostList';
 import PostInput from '../components/PostInput';
 import Layout from '../components/Layout';
+import Auth from '../components/Auth';
 
 const Home = () => {
   return (
     <Layout>
       <main className="mt-32 md:w-9/12 p-3 mx-auto">
+        {/* <Auth /> */}
         <PostInput />
         <PostList />
       </main>
