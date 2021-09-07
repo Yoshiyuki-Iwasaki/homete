b82e08685ee32107371aa517b76bcc5d2fe4943d
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-09-07 22:39:24 +0900

    create user page.

--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import firebase from "../../../firebase/clientApp";
+import Auth from '../../components/Auth';
+import User from '../../components/User';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
-const User = () => {
-  return (
-    <Layout>
-      <p>User</p>
-    </Layout>
-  );
+
+const UserDetail = ({ todo }) => {
+  const [user, loading, error] = useAuthState(firebase.auth());
+
+  if (loading) {
+    return <h6>Loading...</h6>;
+  }
+  if (error) {
+    return null;
+  }
+
+  return <Layout>{!user ? <Auth /> : <User todo={todo} />}</Layout>;
 };
 
-export default User;
+export default UserDetail;
+
+export const getStaticPaths = async () => {
+  const db = firebase.firestore();
+  const res = await db.collection("users").get();
+  const paths = res.docs.map(todo => `/user/${todo.data().uid}`);
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async context => {
+  const db = firebase.firestore();
+  const id = context.params.id;
+  const res = await db.collection("users").get();
+  const todos = res.docs.map(todo => todo.data());
+  const array = todos.find(todo => todo.uid == id);
+  return {
+    props: {
+      todo: array,
+    },
+  };
+};
