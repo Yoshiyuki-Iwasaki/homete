61bd7ce142c1eac246249e69ff04e4261e614aec
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-09-08 18:00:12 +0900

    create tab function.

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from 'react';
 import Follow from './Follow';
 import PostList from './PostList';
 import { useCollection } from 'react-firebase-hooks/firestore';
@@ -7,6 +7,7 @@ import firebase from '../../firebase/clientApp';
 
 const User = ({ todo }) => {
   const db = firebase.firestore();
+  const [openTab, setOpenTab] = useState(1);
   const [list, loading, error] = useCollection(
     db.collection('textList').where('userId', '==', todo.uid),
     {},
@@ -27,7 +28,42 @@ const User = ({ todo }) => {
       </figure>
       <h1 className="mt-3 text-center text-2xl font-bold">{todo.displayName}</h1>
       <Follow userId={todo} />
-      <PostList list={list} />
+      <div className="mt-5 md:w-9/12 mx-auto">
+        <div className="flex justify-center">
+          <button
+            className={`py-3 md:w-1/2 inline-block text-center text-lg ${
+              openTab === 1 ? 'text-pink-700 border-b-4 border-pink-700' : 'text-gray-200'
+            }`}
+            data-toggle="tab"
+            role="tablist"
+            onClick={(e) => {
+              e.preventDefault();
+              setOpenTab(1);
+            }}
+          >
+            投稿
+          </button>
+          <button
+            className={`ml-2 py-3 md:w-1/2 px-12 inline-block text-center text-lg ${
+              openTab === 2 ? 'text-pink-700 border-b-4 border-pink-700' : 'text-gray-200'
+            }`}
+            data-toggle="tab"
+            role="tablist"
+            onClick={(e) => {
+              e.preventDefault();
+              setOpenTab(2);
+            }}
+          >
+            いいね
+          </button>
+        </div>
+        <ul>
+          <li className={openTab === 1 ? 'block' : 'hidden'} id="link1">
+            <PostList list={list} />
+          </li>
+          <li className={openTab === 2 ? 'block' : 'hidden'} id="link2"></li>
+        </ul>
+      </div>
     </div>
   );
 };
