77d5c052f1352143286a1765637cd9b59c38aa26
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-11-02 16:00:19 +0900

    create follow page.

---
+++ src/pages/user/[id]/follow.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const follow = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default follow
---
+++ src/pages/user/[id]/follower.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const follower = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default follower
